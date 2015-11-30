12fd88b21b4a06b4933e21e9d5808ceedb55d9d3
heraut <herautv@gmail.com>
2015-11-29 19:28:56 -0800

    Changed all tabs and indentation to 4 spaces in python source files

--- csvFile.py
+++ csvFile.py
@@ -19,30 +19,30 @@ __CSV_DATE_PAID__ = 18
 __CSV_DELIMETER__ = ','
 
 class CSVRecord:
-   """ Class for a CSV row in the record """
-   def __init__(self,infoRow):
-      self.firstName = infoRow[__CSV_FIRST_NAME__]
-      self.lastName = infoRow[__CSV_LAST_NAME__]
-      self.address = infoRow[__CSV_ADDRESS__]
-      self.city = infoRow[__CSV_CITY__]
-      self.province = infoRow[__CSV_PROVINCE__]
-      self.postalCode = infoRow[__CSV_POSTAL_CODE__]
-      self.amountPaid = infoRow[__CSV_AMOUNT_PAID__]
-      self.datePaid = infoRow[__CSV_DATE_PAID__]
+    """ Class for a CSV row in the record """
+    def __init__(self,infoRow):
+        self.firstName = infoRow[__CSV_FIRST_NAME__]
+        self.lastName = infoRow[__CSV_LAST_NAME__]
+        self.address = infoRow[__CSV_ADDRESS__]
+        self.city = infoRow[__CSV_CITY__]
+        self.province = infoRow[__CSV_PROVINCE__]
+        self.postalCode = infoRow[__CSV_POSTAL_CODE__]
+        self.amountPaid = infoRow[__CSV_AMOUNT_PAID__]
+        self.datePaid = infoRow[__CSV_DATE_PAID__]
 
 # Main APIs
 def openCsvFile(filename):
-   """ Return the CSV file as an iterator object """
+    """ Return the CSV file as an iterator object """
 
-   try:
-      csvFile = open(filename, 'rb')
-   except IOError:
-      print ("Error in opening/reading CSV file. Check if file exists...")
-      return False
+    try:
+        csvFile = open(filename, 'rb')
+    except IOError:
+        print ("Error in opening/reading CSV file. Check if file exists...")
+        return False
 
-   csvOutput = csv.reader(csvFile, delimiter=__CSV_DELIMETER__)
+    csvOutput = csv.reader(csvFile, delimiter=__CSV_DELIMETER__)
    
-   return csvOutput
+    return csvOutput
 
 
 def openCsvWriter(filename):
@@ -51,8 +51,8 @@ def openCsvWriter(filename):
     try:
         outputCsvFile = open(filename, 'wb')
     except IOError:
-      print ("Error in opening output CSV file.") 
-      return False, False
+        print ("Error in opening output CSV file.") 
+        return False, False
 
     csvOutput = csv.writer(outputCsvFile, delimiter=__CSV_DELIMETER__)
 
--- main.py
+++ main.py
@@ -10,84 +10,84 @@ from datetime import datetime
 # note that the module name will change  to configparser for Python3
 import ConfigParser
 
-CONFIGURATION_FILE	= "donation_import.cfg"
-DEFAULT_HOST 		= "localhost"
-DEFAULT_USER 		= "root"
+CONFIGURATION_FILE    = "donation_import.cfg"
+DEFAULT_HOST         = "localhost"
+DEFAULT_USER         = "root"
 #stub-start
 #erase this!
-DEFAULT_PASS 		= "password"
+DEFAULT_PASS         = "password"
 #stub-end
-DEFAULT_DBNAME 		= "DatabaseName"
+DEFAULT_DBNAME         = "DatabaseName"
 DEFAULT_CSVFILENAME = "CharityDataDownload.csv"
 
 def readConfigurationFile(file):
-	"""Reads database and csv file parameters from a given file """
-	databaseSectionName = 'database';
-	config = ConfigParser.ConfigParser()
-	config.read('donation_import.cfg')
-	host = config.get(databaseSectionName,'host')
-	userName = config.get(databaseSectionName,'userName')
-	password = config.get(databaseSectionName,'password')
-	dbName = config.get(databaseSectionName,'dbName')
+    """Reads database and csv file parameters from a given file """
+    databaseSectionName = 'database';
+    config = ConfigParser.ConfigParser()
+    config.read('donation_import.cfg')
+    host = config.get(databaseSectionName,'host')
+    userName = config.get(databaseSectionName,'userName')
+    password = config.get(databaseSectionName,'password')
+    dbName = config.get(databaseSectionName,'dbName')
 
-	csvFileName = config.get('csvFile','csvFileName')
-        csvErrorFileName = config.get('csvFile','csvErrorFileName')
+    csvFileName = config.get('csvFile','csvFileName')
+    csvErrorFileName = config.get('csvFile','csvErrorFileName')
 
-	# Following line is only used for debugging
-	#print(",".join((host,userName,password,dbName,csvFileName)))
+    # Following line is only used for debugging
+    #print(",".join((host,userName,password,dbName,csvFileName)))
 
-	return (host,userName,password,dbName,csvFileName,csvErrorFileName)
+    return (host,userName,password,dbName,csvFileName,csvErrorFileName)
 
 
 def changeCSVToDatabaseFormat(csvRecord):
-	""" Change the CSV format to a more suitable format for database addition """
+    """ Change the CSV format to a more suitable format for database addition """
 
-	donorInfo = database.DonorInfo()
-	donorInfo.firstName = csvRecord.firstName
-	donorInfo.lastName = csvRecord.lastName
-	donorInfo.address = csvRecord.address
-	donorInfo.city = csvRecord.city
-	donorInfo.province = csvRecord.province
-	donorInfo.postalCode = csvRecord.postalCode
-	donorInfo.amountPaid = float(csvRecord.amountPaid)
-	donorInfo.datePaid = datetime.strptime(csvRecord.datePaid,'%m/%d/%Y')
+    donorInfo = database.DonorInfo()
+    donorInfo.firstName = csvRecord.firstName
+    donorInfo.lastName = csvRecord.lastName
+    donorInfo.address = csvRecord.address
+    donorInfo.city = csvRecord.city
+    donorInfo.province = csvRecord.province
+    donorInfo.postalCode = csvRecord.postalCode
+    donorInfo.amountPaid = float(csvRecord.amountPaid)
+    donorInfo.datePaid = datetime.strptime(csvRecord.datePaid,'%m/%d/%Y')
 
-	return donorInfo
+    return donorInfo
 
 
 # Main function 
 def executeAddTransaction(csvFileName, dbInfo, csvOutputWriter):
         
-        csvRows = csvFile.getRows(csvFileName)
+    csvRows = csvFile.getRows(csvFileName)
 
-        if not(csvRows) or not(csvOutputWriter) :
-            print("input or output CSV file cannot be opened.\nProcessing stopped.")
-            return
-	#csvObject = csvFile.openCsvFile(csvFileName)
-	#if csvObject == False:
-	#	return False
-	#csvRecords = csvFile.getRecords(csvObject)
+    if not(csvRows) or not(csvOutputWriter) :
+        print("input or output CSV file cannot be opened.\nProcessing stopped.")
+        return
+    #csvObject = csvFile.openCsvFile(csvFileName)
+    #if csvObject == False:
+    #    return False
+    #csvRecords = csvFile.getRecords(csvObject)
 
-	for i,csvRow in enumerate(csvRows):
-            if(i>0):
-                try:
-                    csvRecord = csvFile.CSVRecord(csvRow)
-		    donorInfo = changeCSVToDatabaseFormat(csvRecord)
-		    database.addTransactionToDatabase(donorInfo, dbInfo)
-                except ValueError as e:
-                    csvOutputWriter.writerow(csvRow)
-                    print("Conversion Error:\n{0}\nwhen dealing with record:{1}".format(e, csvRow))
-                except KeyboardInterrupt as e:
-                    csvOutputWriter.writerow(csvRow)
-                    print("Operation interrupted by operator while dealing with record:{1}".format(csvRow))
-                    raise # we re-raise keyboard interruption to allow interruption of the program
-                except Exception as e:
-                    csvOutputWriter.writerow(csvRow)
-                    print("Program met Exception: {0} when dealing with record {1}".format(e, csvRow))
-            else: #i=0
-                # the first rows contains the headers, which we just copy
-                # to the output CSV file
+    for i,csvRow in enumerate(csvRows):
+        if(i>0):
+            try:
+                csvRecord = csvFile.CSVRecord(csvRow)
+                donorInfo = changeCSVToDatabaseFormat(csvRecord)
+                database.addTransactionToDatabase(donorInfo, dbInfo)
+            except ValueError as e:
                 csvOutputWriter.writerow(csvRow)
+                print("Conversion Error:\n{0}\nwhen dealing with record:{1}".format(e, csvRow))
+            except KeyboardInterrupt as e:
+                csvOutputWriter.writerow(csvRow)
+                print("Operation interrupted by operator while dealing with record:{1}".format(csvRow))
+                raise # we re-raise keyboard interruption to allow interruption of the program
+            except Exception as e:
+                csvOutputWriter.writerow(csvRow)
+                print("Program met Exception: {0} when dealing with record {1}".format(e, csvRow))
+        else: #i=0
+            # the first rows contains the headers, which we just copy
+            # to the output CSV file
+            csvOutputWriter.writerow(csvRow)
 
 
 # Call the main command
