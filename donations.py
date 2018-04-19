e79ce3ffff9efafc395948913631c8ca3be9ea5b
Wai-Kin Chau <waikinarthur@gmail.com>
2018-04-19 16:30:04 -0700

    Changed date format and add a new column in donations.

--- donations.py
+++ donations.py
@@ -1,12 +1,18 @@
 #!/bin/usr/python
+""" Create a CSV to import into CiviCRM from CanadaHelps output CSV.
+
+Fields order or their existances are not an issue with this script. But this
+does not mean resulting csv file will import into CiviCRM successfully.
+"""
 
 import csv
 import sys
+import datetime
 
 # Fields to export
 FORMAL_TITLE="DONOR TITLE"
-FIRST_NAME="DONOR FIRST NAME"
-LAST_NAME="DONOR LAST NAME"
+FIRST_NAME="DONOR FIRST NAME" #REQUIRED
+LAST_NAME="DONOR LAST NAME" #REQUIRED
 COMPANY_NAME="DONOR COMPANY NAME"
 ADDRESS="DONOR ADDRESS 1"
 SUPPLEMENTAL_ADDRESS_1="DONOR ADDRESS 2"
@@ -15,21 +21,15 @@ STATE="DONOR PROVINCE/STATE"
 POSTAL_CODE="DONOR POSTAL/ZIP CODE"
 COUNTRY="DONOR COUNTRY"
 PHONE="DONOR PHONE NUMBER"
-EMAIL="DONOR EMAIL ADDRESS"
+EMAIL="DONOR EMAIL ADDRESS" #REQUIRED
 
-EXTERNAL_ID="DONOR EMAIL ADDRESS"
+EXTERNAL_ID="DONOR EMAIL ADDRESS" #REQUIRED
 
 INVOICE_NUMBER="TRANSACTION NUMBER"
-TOTAL_AMOUNT="AMOUNT"
+TOTAL_AMOUNT="AMOUNT" #REQUIRED
 DATE_RECEIVED="DONATION DATE"
 SOURCE="DONATION SOURCE"
 NOTE="MESSAGE TO CHARITY"
-
-# the order and which fields to export
-INDIVIDUAL_CONTACT=(EXTERNAL_ID, FORMAL_TITLE, FIRST_NAME, LAST_NAME, ADDRESS, SUPPLEMENTAL_ADDRESS_1, CITY, STATE, POSTAL_CODE, COUNTRY, PHONE, EMAIL)
-INDIVIDUAL_DONATION=(EXTERNAL_ID, INVOICE_NUMBER, TOTAL_AMOUNT, DATE_RECEIVED, SOURCE, NOTE)
-ORGANIZATION_CONTACT=(EXTERNAL_ID, COMPANY_NAME, ADDRESS, SUPPLEMENTAL_ADDRESS_1, CITY, STATE, POSTAL_CODE, COUNTRY, PHONE, EMAIL)
-ORGANIZATION_DONATION=(EXTERNAL_ID, INVOICE_NUMBER, TOTAL_AMOUNT, DATE_RECEIVED, SOURCE, NOTE)
 
 def export(fileName, outputFolder):
 	""" Export the four files
@@ -48,33 +48,74 @@ def export(fileName, outputFolder):
 			contact = []
 			donation = []
 			if row[COMPANY_NAME] == '' or row[COMPANY_NAME] == 'Anon':
-				ind_contacts.append(fillList(row, INDIVIDUAL_CONTACT))
-				ind_donations.append(fillList(row, INDIVIDUAL_DONATION))
+				ind_contacts.append(fill_individual_contract(row))
+				ind_donations.append(fill_donation(row))
 			else:
-				org_contacts.append(fillList(row, ORGANIZATION_CONTACT))
-				org_donations.append(fillList(row, ORGANIZATION_DONATION))
-	outputFile(outputFolder + "/individual_contracts.csv", ind_contacts)
-	outputFile(outputFolder + "/individual_donations.csv", ind_donations)
-	outputFile(outputFolder + "/organization_contracts.csv", org_contacts)
-	outputFile(outputFolder + "/organization_donations.csv", org_donations)
+				org_contacts.append(fill_organization_contract(row))
+				org_donations.append(fill_donation(row))
+	output_file(outputFolder + "/individual_contracts.csv", ind_contacts)
+	output_file(outputFolder + "/individual_donations.csv", ind_donations)
+	output_file(outputFolder + "/organization_contracts.csv", org_contacts)
+	output_file(outputFolder + "/organization_donations.csv", org_donations)
 
-def fillList(row, keys):
-	""" Create a list with the database row and list of table key
+def fill_individual_contract(row):
+	""" Create a csv row for individual contact.
 
 	Argument:
-		row  -- (Dictionary) the row of database
-		keys -- (List)       the keys to extract the data
+		row -- (Dictionary) the row extract data from
 	"""
+	contact = []
+	contact.append(row[EXTERNAL_ID])
+	contact.append(row[FORMAL_TITLE])
+	contact.append(row[FIRST_NAME])
+	contact.append(row[LAST_NAME])
+	contact.append(row[ADDRESS])
+	contact.append(row[SUPPLEMENTAL_ADDRESS_1])
+	contact.append(row[CITY])
+	contact.append(row[STATE])
+	contact.append(row[POSTAL_CODE])
+	contact.append(row[COUNTRY])
+	contact.append(row[PHONE])
+	contact.append(row[EMAIL])
+	return contact
 
-	answer = []
-	for key in keys:
-		if key in row:
-			answer.append(row[key])
-		else:
-			answer.append("")
-	return answer
+def fill_organization_contract(row):
+	""" Create a csv row for organization contact.
 
-def outputFile(fileName, items):
+	Argument:
+		row -- (Dictionary) the row extract data from
+	"""
+	contact = []
+	contact.append(row[EXTERNAL_ID])
+	contact.append(row[COMPANY_NAME])
+	contact.append(row[ADDRESS])
+	contact.append(row[SUPPLEMENTAL_ADDRESS_1])
+	contact.append(row[CITY])
+	contact.append(row[STATE])
+	contact.append(row[POSTAL_CODE])
+	contact.append(row[COUNTRY])
+	contact.append(row[PHONE])
+	contact.append(row[EMAIL])
+	return contact
+
+def fill_donation(row):
+	""" Create a csv row for donation.
+
+	Argument:
+		row -- (Dictionary) the row extract data from
+	"""
+	donation = []
+	donation.append(row[EXTERNAL_ID])
+	donation.append(row[INVOICE_NUMBER])
+	donation.append(row[TOTAL_AMOUNT])
+	date = datetime.datetime.strptime(row[DATE_RECEIVED], '%Y/%m/%d').strftime('%Y-%m-%d')
+	donation.append(date)
+	donation.append(row[SOURCE])
+	donation.append(row[NOTE])
+	donation.append("Donation")
+	return donation
+
+def output_file(fileName, items):
 	""" Output a csv file
 
 	Argument:
